498b2017c73cff989078267cfe2b7b67ae926155
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2019-09-27 10:47:15 +0200

    второе занятие Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>test</p>
+    <!-- HERE: non dynamic props pass-->
+    <HelloWorld msg="Hello" v-on:customEvent="onCustomEvent"/>
+    <!-- HERE: dynamic  props pass -->
+    <HelloWorld v-bind:msg="secondTitle" v-on:customEvent="onCustomEvent"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+//import MyFancyModal from 'npm-package'
 
 export default {
   name: 'app',
+
+  data() {
+    return {
+      secondTitle: 'Salut',
+      links: ['https://codepen.io/shershen08/pen/exzMzr?editors=1011']
+    }
+  },
+
+  methods: {
+    // HERE: method that is called on 
+    // v-on:customEvent from template
+    onCustomEvent(data){
+      console.log(data)
+    }
+  },
+
   components: {
-    HelloWorld
+    HelloWorld, // >> <hello-world/> || <HelloWorld/>
+    
+    //HERE: renaming components
+    //MyFancyModal,
+    //'app-modal-v2': MyFancyModal
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyLink.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <a v-bind:href="url">{{origin(url)}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-link',
+  props: {
+    url: String
+  },
+//   computed: {
+//       origin(){
+//           return new URL(this.url).origin
+//       }
+//   }
+  methods: {
+      origin(url){
+          return new URL(this.url).origin
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
